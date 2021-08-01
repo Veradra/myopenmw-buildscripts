f4eb2b61d8d71c4ae7104862e80128806ba98b86
Veradra <31333156+Veradra@users.noreply.github.com>
2021-08-01 12:49:38 -0500

    Update setup.sh
    Would be useful if the script actually worked, eh?

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 
 echo building build env, might take howeverthefuck long.
 CI/before_script.msvc.sh -k -p Win64 -v 2019 -N
-cp postbuild.sh MSVC2019_64_Ninja
+cp build.sh MSVC2019_64_Ninja
 cd MSVC2019_64_Ninja
-./postbuild.sh
+./build.sh
