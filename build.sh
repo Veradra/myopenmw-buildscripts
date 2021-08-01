1b0f680fb7e53ad596b53287784c5f6c6caed965
Veradra <31333156+Veradra@users.noreply.github.com>
2021-08-01 11:30:49 -0500

    renamed the files to make more sense.
    build.sh -> setup.sh
    postbuild.sh -> build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,55 @@
 #!/bin/bash
+source activate_msvc.sh
 
-echo building build env, might take howeverthefuck long.
-CI/before_script.msvc.sh -k -p Win64 -v 2019 -N
-cp postbuild.sh MSVC2019_64_Ninja
-cd MSVC2019_64_Ninja
-./postbuild.sh
+release() {
+    echo "Building 'Release', output will be in the Release folder."
+    ninja -f build-Release.ninja
+    exit
+}
+
+debug() {
+    echo "Building 'Debug', output will be in the Debug folder."
+    ninja -f build-Debug.ninja
+    exit
+}
+
+relwithdebinfo() {
+    echo "Building 'RelWithDebInfo', output will be in the RelWithDebInfo folder"
+    ninja -f build-RelWithDebInfo.ninja
+    exit
+}
+
+all() {
+    echo "Building 'all', why?"
+    ninja -f build-Release.ninja
+    ninja -f build-Debug.ninja
+    ninja -f build-RelWithDebInfo.ninja
+    exit
+}
+
+
+echo "Choose buildtype"
+echo
+echo "1) Release"
+echo "2) Debug"
+echo "3) RelWithDebInfo"
+echo "4) All"
+
+read choice
+
+if [ "$choice" = "1" ]
+then
+    release
+elif [ "$choice" = "2" ]
+then
+    debug
+elif [ "$choice" = "3" ]
+then
+    relwithdebinfo
+elif [ "$choice" = "4" ]
+then
+    all
+else
+    echo "?????"
+    exit
+fi
