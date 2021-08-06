6329382051f5df4ac7aec88e9c79c6994a28fa45
Veradra <31333156+Veradra@users.noreply.github.com>
2021-08-06 18:45:59 -0500

    updated setup.sh
    Now includes "git pull" to update the local repository to the latest version.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-echo building build env, might take howeverthefuck long.
+echo "Pulling an updated version of the git repository."
+git pull
+echo "Building build env, might take howeverthefuck long."
 CI/before_script.msvc.sh -k -p Win64 -v 2019 -N
 cp build.sh MSVC2019_64_Ninja
 cd MSVC2019_64_Ninja
