78d526c9c68e17cccf396268bd5112de3c560a0b
Veradra <31333156+Veradra@users.noreply.github.com>
2021-08-06 21:11:42 -0500

    changed build.sh
    uses case instead of an elif ladder. Unlikely to make any major difference, but I honestly am using this more as a learning experience than anything

--- build.sh
+++ build.sh
@@ -35,21 +35,12 @@ echo "2) Debug"
 echo "3) RelWithDebInfo"
 echo "4) All"
 
-read choice
+read -t 10 choice
 
-if [ "$choice" = "1" ]
-then
-    release
-elif [ "$choice" = "2" ]
-then
-    debug
-elif [ "$choice" = "3" ]
-then
-    relwithdebinfo
-elif [ "$choice" = "4" ]
-then
-    all
-else
-    echo "?????"
-    exit
-fi
+case $choice in
+    "1")release;;
+    "2")debug;;
+    "3")relwithdebinfo;;
+    "4")all;;
+    *)echo "No valid selection made, exiting";;
+esac
